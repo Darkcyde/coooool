d438d724ee5e80b265f9b225c4faad457c12b107
Darkcyde <56904224+Darkcyde@users.noreply.github.com>
2019-10-29 08:52:27 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,16 +1,30 @@
-def Calc():
-    num1 = input("Enter first number\n")
-    num1 = float(num1)
-    num2 = input("Enter operation\n")
-    num3 = input("Enter second number\n")
-    num3 = float(num3)
+# Input definitions
 
-    if num2 == "-":
+def Calc():
+    print("Enter num")
+    num1 = input("Enter first number\n")
+    num1 = int(num1)
+    op = input("Enter operation (+, -, /, ^, * or x)\n")
+    num3 = input("Enter second number\n")
+    num3 = int(num3)
+        
+    # Calculations    
+    
+    if op == "-":                 # Subtraction
         both = num1-num3
-    if num2 == "+":
+    if op == "+":                 # Addition
         both = num1+num3
-    if num2 == "*" or "x":
+    if op == "*" or "x":          # Multiplication
         both = num1*num3
-    if num2 == "/":
-        both = num1/num3
-    print("Your anwser is " + str(both))
+    if op == "/":                 # Division
+        if num1 > 0 and num3 > 0:
+            both = num1/num3
+        else:
+            print ("Cannot divide by zero")
+            exit()
+    if op == "^":                 # Powers
+        both = num1**num3
+
+    # Output
+    output = both    
+    print("Your anwser is " + str(output))
